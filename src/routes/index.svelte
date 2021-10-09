61401cc72684f5724d214e6f5bc7d2dc51803edb
fuurin <komatsuemgbr0624@gmail.com>
2021-10-09 17:54:40 +0900

    pokedex init

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { pokemon } from '../stores/pokestore'
+ 
+  console.log($pokemon)
+</script>
+
+<svelte:head>
+  <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>Svelte Kit Pokedex</h1>
+{#each $pokemon as pokeman}
+  <p>{pokeman.name}</p>  
+{/each}
