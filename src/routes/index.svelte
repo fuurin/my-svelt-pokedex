9b664e71afa39e853ea4a1febe754b4fdc1b6886
fuurin <komatsuemgbr0624@gmail.com>
2021-10-11 00:09:11 +0900

    写経完了

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,37 @@
+
+<script context="module">
+  export async function load({page}) {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`
+    const res = await fetch(url)
+    const data = await res.json()
+    const loadedPokemon = data.results.map((data, index) => {
+      return {
+        name: data.name,
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+      }
+    })
+    return { props: { pokemon: loadedPokemon } }
+  }
+</script>
+
 <script>
-  import { pokemon } from '../stores/pokestore'
   import PokemanCard from '../components/pokemanCard.svelte'
- 
-  console.log($pokemon)
+  
+  export let pokemon
+
+  let searchTerm = ""
+  let filteredPokemon = []
+  
+  $: {
+    if (searchTerm) {
+      filteredPokemon = pokemon.filter(pokeman => 
+        pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    } else {
+      filteredPokemon = [ ...pokemon ]
+    }
+  }
 </script>
 
 <svelte:head>
@@ -13,8 +42,13 @@
   Svelte Kit Pokedex
 </h1>
 
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+       type="text"
+       placeholder="Search Pokemon"
+       bind:value={searchTerm}>
+
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-  {#each $pokemon as pokeman}
+  {#each filteredPokemon as pokeman}
     <PokemanCard pokeman={pokeman} />
   {/each}
 </div>
